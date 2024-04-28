27005099cb3c151a0aa746631a8479651eef3b48
rcl <ronanlawlor2001@gmail.com>
2024-04-28 17:38:49 +0100

    Add different repetition lengths in tree

--- src/parser.rs
+++ src/parser.rs
@@ -55,17 +55,101 @@ impl RegExParser {
         if self.more() {
             match self.peek() {
                 '*' => {
-                    self.consume('*').unwrap();
+                    self.consume('*')?;
                     Ok(RegEx::Repetition(RepetitionType::OrMore(0), Box::new(b)))
                 }
                 '+' => {
-                    self.consume('+').unwrap();
+                    self.consume('+')?;
                     Ok(RegEx::Repetition(RepetitionType::OrMore(1), Box::new(b)))
                 }
+                '?' => {
+                    self.consume('?')?;
+                    Ok(RegEx::Repetition(RepetitionType::ZeroOrOne, Box::new(b)))
+                }
+                '{' => Ok(RegEx::Repetition(self.repetition_group()?, Box::new(b))),
                 _ => Ok(b),
             }
         } else {
             Ok(b)
+        }
+    }
+
+    /// Find the type of repetition present
+    fn repetition_group(&mut self) -> Result<RepetitionType, String> {
+        self.consume('{')?;
+        let mut min_count: u32 = 0;
+        let mut max_count: Option<u32> = None;
+        let mut two_num: bool = false;
+        // Capture minimum count
+        while self.more() && self.peek() != '}' {
+            match self.peek() {
+                num @ '0'..='9' => {
+                    self.consume(num)?;
+                    // Multiply by 10 to account for more than 1 digit numbers
+                    min_count = min_count * 10
+                        + num
+                            .to_digit(10)
+                            .expect("Current char already checked to be in '0'..='9'");
+                }
+                ',' => {
+                    self.consume(',')?;
+                    two_num = true;
+                    break;
+                }
+                e @ _ => {
+                    return Err(format!(
+                        "Char '{}' not valid number for repetition group",
+                        e
+                    ));
+                }
+            }
+        }
+        // If maximum count is present, try to capture it
+        while self.more() && self.peek() != '}' {
+            match self.peek() {
+                num @ '0'..='9' => {
+                    match max_count {
+                        Some(count) => {
+                            max_count = Some(
+                                10 * count
+                                    + self
+                                        .peek()
+                                        .to_digit(10)
+                                        .expect("Current char already checked to be in '0'..='9'"),
+                            )
+                        }
+                        None => {
+                            max_count = Some(
+                                self.peek()
+                                    .to_digit(10)
+                                    .expect("Current char already checked to be in '0'..='9'"),
+                            )
+                        }
+                    };
+                    self.consume(num)?;
+                }
+                e @ _ => {
+                    return Err(format!(
+                        "Char '{}' not valid number for repetition group",
+                        e
+                    ));
+                }
+            }
+        }
+
+        if self.more() && self.peek() == '}' {
+            self.consume('}')?;
+        }
+        // Return final repetition type based on numbers found
+        match max_count {
+            Some(max_count) => Ok(RepetitionType::Between(min_count, max_count)),
+            None => {
+                if two_num {
+                    Ok(RepetitionType::OrMore(min_count))
+                } else {
+                    Ok(RepetitionType::Exactly(min_count))
+                }
+            }
         }
     }
 
@@ -151,6 +235,35 @@ mod test {
                     Box::new(Element(vec![Box::new(Terminal('a'))])),
                     Box::new(Element(vec![Box::new(Terminal('b'))]))
                 ))
+            ))])
+        );
+    }
+
+    #[test]
+    fn test_hard_regex() {
+        let mut parser = RegExParser::new("a{8}".to_string());
+        assert_eq!(
+            parser.parse().unwrap(),
+            Element(vec![Box::new(Repetition(
+                RepetitionType::Exactly(8),
+                Box::new(Terminal('a'))
+            ))])
+        );
+        let mut parser = RegExParser::new("a{5,}".to_string());
+        assert_eq!(
+            parser.parse().unwrap(),
+            Element(vec![Box::new(Repetition(
+                RepetitionType::OrMore(5),
+                Box::new(Terminal('a'))
+            ))])
+        );
+
+        let mut parser = RegExParser::new("a{1,10}".to_string());
+        assert_eq!(
+            parser.parse().unwrap(),
+            Element(vec![Box::new(Repetition(
+                RepetitionType::Between(1, 10),
+                Box::new(Terminal('a'))
             ))])
         );
     }
