379713e1f18c64f50383266b54aa6e2f382f4945
rcl <ronanlawlor2001@gmail.com>
2024-05-24 20:42:18 +0100

    Fix incorrect directory

--- build.lua
+++ build.lua
@@ -2,7 +2,7 @@ vim.schedule(
     function()
         local home_dir = os.getenv("XDG_CONFIG_HOME") or "~/.local/share"
         local install_dir = string.format("{}/lazy/regex-railroad", home_dir)
-        local utils = require(string.format("{}/regex-railroad.utils", install_dir))
+        local utils = require(string.format("{}/lua/regex-railroad.utils", install_dir))
         utils.install_binary()
     end
 )
