ba797b7c5f9cc26c2819318cba5c048c952e99b3
rcl <ronanlawlor2001@gmail.com>
2024-05-25 23:25:14 +0100

    Update permissions on downloaded executable

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-output=$(wget -q -O $1/regex-railroad https://github.com/rclawlor/regex-railroad.nvim/releases/download/$2/regex-railroad)
+output=$(wget -q -O $1/regex-railroad https://github.com/rclawlor/regex-railroad.nvim/releases/download/$2/regex-railroad && chmod a+x $1/regex-railroad)
 exit $?
 
