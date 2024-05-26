12856a8ed9efc3db253e9ed71f2c17618b3d01ef
rcl <ronanlawlor2001@gmail.com>
2024-05-26 09:20:11 +0100

    Add dev option to use locally compiled rust binary

--- lua/regex-railroad/consts.lua
+++ lua/regex-railroad/consts.lua
@@ -1,20 +1,32 @@
 local M = {}
 
--- Variables
+--- User home directory
 M.home_directory = string.format(
     "%s", os.getenv("HOME")
 )
 
+--- Root directory of plugin
 M.root_directory = string.format(
     "%s/nvim/lazy/regex-railroad.nvim",
     os.getenv("XDG_CONFIG_HOME") or string.format("%s/.local/share", M.home_directory)
 )
 
+--- Rust binary location
 M.binary_location = string.format(
     "%s/regex-railroad",
     M.root_directory
 )
 
+--- Working directory for development
+M.dev_directory = vim.fn.getcwd()
+
+--- Rust binary location for development
+M.dev_binary_location = string.format(
+    "%s/target/release/regex-railroad",
+    M.dev_directory
+)
+
+--- A mapping of wget error codes to useful user message
 M.wget_errors = {
     [3] = "I/O error when writing %s regex-raiload binary",
     [4] = "Network failure when downloading %s regex-railroad binary",
