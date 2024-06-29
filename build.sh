fc97fbfec9c273fcc4681016daea3358fe1a433d
rcl <ronanlawlor2001@gmail.com>
2024-06-29 10:49:53 +0100

    Fix incorrect shell variable

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$URL = https://github.com/rclawlor/regex-railroad.nvim/releases/download/$2/regex-railroad-$3
+URL=https://github.com/rclawlor/regex-railroad.nvim/releases/download/$2/regex-railroad-$3
 
 output=$(wget -q -O $1/regex-railroad-$3 $URL && chmod a+x $1/regex-railroad-$3)
 exit $?
